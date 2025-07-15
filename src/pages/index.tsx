fa47b2aa3a1dc7ae77dac0097a7cf43cf5b52427
jorabek710ly <jorabekabdurashidov555@gmail.com>
2025-07-15 11:47:10 +0500

    css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,10 @@ import React from 'react';
 import { useRoutes } from 'react-router-dom';
 import Layout from './layout/Layout';
 import Home from './home/Home';
-
 import Login from './login/Login';
 import Auth from './auth/Auth';
 // import Stats from './statistics/Statistics';
 import Statistics from './statistics/Statistics';
-
-
-
-
-
 const MainRoutes = () => {
   const routes = useRoutes([
     {
